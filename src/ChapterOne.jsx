5441706573971b5fe54c8969ef478012e616c9a2
marcinlowercase <marcinlowercase@gmail.com>
2024-11-07 14:22:30 -0600

    init2

--- src/ChapterOne.jsx
+++ src/ChapterOne.jsx
@@ -1,18 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import sky from './assets/sky.svg'
-import mountains from './assets/mountains.svg'
-import './ChapterOne.css'
+import { useState } from "react";
+import sky from "./assets/sky.svg";
+import cave from "./assets/cave.svg";
+import mountains from "./assets/mountains.svg";
+import "./ChapterOne.css";
 
 function ChapterOne() {
-    return (
-        <>
-            <img src={sky} id='sky'></img>
-            <img src={mountains} id='mountains'></img>
-            
-        </>
-    )
+  return (
+    <>
+      <img src={sky} id="sky"></img>
+      <img src={cave} id="cave"></img>
+
+      {/* <img src={mountains} id="mountains"></img> */}
+    </>
+  );
 }
 
-export default ChapterOne
+export default ChapterOne;
