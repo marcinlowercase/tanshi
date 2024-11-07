acb61a48ae3ef5525c1a97a7dc890794a4f156bd
marcinlowercase <marcinlowercase@gmail.com>
2024-11-07 17:00:04 +0000

    init  commit

---
+++ src/ChapterOne.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import sky from './assets/sky.svg'
+import mountains from './assets/mountains.svg'
+import './ChapterOne.css'
+
+function ChapterOne() {
+    return (
+        <>
+            <img src={sky} id='sky'></img>
+            <img src={mountains} id='mountains'></img>
+            
+        </>
+    )
+}
+
+export default ChapterOne
