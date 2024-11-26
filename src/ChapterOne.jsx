5a680613d1563dcc1ddf8a5022eb88c5149edcce
marcinlowercase <marcinlowercase@gmail.com>
2024-11-26 12:19:30 -0600

    stuck sabe

--- src/ChapterOne.jsx
+++ src/ChapterOne.jsx
@@ -2,19 +2,18 @@ import { useState } from "react";
 import sky from "./assets/sky.svg";
 import cave from "./assets/cave.svg";
 import lake from "./assets/lake.svg";
-import forestground from "./assets/forestground.svg"
-import mountains from "./assets/mountains.svg";
+
+import Sabe from "./Sabe";
+
 import "./ChapterOne.css";
 
 function ChapterOne() {
   return (
     <>
-      <img src={forestground} id="forestground"></img>
       <img src={sky} id="sky"></img>
       <img src={cave} id="cave"></img>
       <img src={lake} id="lake"></img>
-
-      {/* <img src={mountains} id="mountains"></img> */}
+      <Sabe></Sabe>
     </>
   );
 }
---
+++ src/Sabe.jsx
@@ -0,0 +1,12 @@
+import forestground from "./assets/forestground.svg";
+import backgroundtree from "./assets/backgroundtree.png";
+
+function Sabe() {
+  return (
+    <div id="sabe">
+      <img src={forestground} id="forestground"></img>
+      <img src={backgroundtree} id="backgroundtree"></img>
+    </div>
+  );
+}
+export default Sabe;
