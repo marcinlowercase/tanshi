98ab2f611cac1971e10147e98cb3c813c8259bb1
marcinlowercase <marcinlowercase@gmail.com>
2024-11-26 16:49:02 +0000

    start-sobey

--- src/ChapterOne.jsx
+++ src/ChapterOne.jsx
@@ -2,15 +2,17 @@ import { useState } from "react";
 import sky from "./assets/sky.svg";
 import cave from "./assets/cave.svg";
 import lake from "./assets/lake.svg";
+import forestground from "./assets/forestground.svg"
 import mountains from "./assets/mountains.svg";
 import "./ChapterOne.css";
 
 function ChapterOne() {
   return (
     <>
+      <img src={forestground} id="forestground"></img>
       <img src={sky} id="sky"></img>
       <img src={cave} id="cave"></img>
-      {/* <img src={lake} id="lake"></img> */}
+      <img src={lake} id="lake"></img>
 
       {/* <img src={mountains} id="mountains"></img> */}
     </>
