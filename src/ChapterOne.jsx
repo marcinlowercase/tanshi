bab20edc2af1940f14390e1bfaa9b1ee09e161d8
marcinlowercase <marcinlowercase@gmail.com>
2024-11-26 08:22:32 -0600

    update lake background

--- src/ChapterOne.jsx
+++ src/ChapterOne.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import sky from "./assets/sky.svg";
 import cave from "./assets/cave.svg";
+import lake from "./assets/lake.svg";
 import mountains from "./assets/mountains.svg";
 import "./ChapterOne.css";
 
@@ -9,6 +10,7 @@ function ChapterOne() {
     <>
       <img src={sky} id="sky"></img>
       <img src={cave} id="cave"></img>
+      {/* <img src={lake} id="lake"></img> */}
 
       {/* <img src={mountains} id="mountains"></img> */}
     </>
