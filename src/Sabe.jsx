f7c3020dc7506344e65027d8776cb6c5681a89e3
marcinlowercase <marcinlowercase@gmail.com>
2024-11-26 15:50:46 -0600

    good sabe

--- src/Sabe.jsx
+++ src/Sabe.jsx
@@ -1,11 +1,13 @@
 import forestground from "./assets/forestground.svg";
 import backgroundtree from "./assets/backgroundtree.png";
+import sabe from "./assets/sabe.svg";
 
 function Sabe() {
   return (
-    <div id="sabe">
+    <div id="sabearea">
       <img src={forestground} id="forestground"></img>
       <img src={backgroundtree} id="backgroundtree"></img>
+      <img src={sabe} id="sabe"></img>
     </div>
   );
 }
