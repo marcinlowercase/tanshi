c71d89fd9e854f59dc110655a73eecc5be120b16
marcinlowercase <marcinlowercase@gmail.com>
2025-05-07 11:50:44 -0600

    deploy failed

--- src/components/chapter_map_top_bar/ChapterMapTopBar.jsx
+++ src/components/chapter_map_top_bar/ChapterMapTopBar.jsx
@@ -1,12 +1,13 @@
-
-import "./chapter-map-top-bar.css"
+import "./chapter-map-top-bar.css";
 const ChapterMapTopBar = () => {
-    return (
-        <div id = "chapter-map-top-bar">
-            <div id={"home-button"}>Home</div>
-            <div>Pick an Animal for a Story!</div>
-            <div>Sign in</div>
-        </div>
-    )
-}
+  return (
+    <div id="chapter-map-top-bar">
+      <a href={"/"} id={"home-button"}>
+        Home
+      </a>
+      <div>Pick an Animal for a Story!</div>
+      <div>Sign in</div>
+    </div>
+  );
+};
 export default ChapterMapTopBar;
