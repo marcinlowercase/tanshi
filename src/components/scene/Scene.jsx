2d8c19082837c495d903b42e13e8d3fec9a93968
marcinlowercase <marcinlowercase@gmail.com>
2025-05-11 20:50:08 -0600

    remove startquestionscript

--- src/components/scene/Scene.jsx
+++ src/components/scene/Scene.jsx
@@ -92,7 +92,7 @@ const Scene = (props) => {
                     volume: 1,
 
                 });
-                startQuestionScript();
+                // startQuestionScript();
             }
         }
 
